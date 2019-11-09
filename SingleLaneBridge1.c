a5aeb6e6f985616a8420cce98d78dbc285d45d49
papageo-dev <40474655+papageo-dev@users.noreply.github.com>
2019-11-09 20:59:24 +0200

    Update SingleLaneBridge1.c
    Change some coments in code file.

--- SingleLaneBridge1.c
+++ SingleLaneBridge1.c
@@ -3,91 +3,91 @@
 #include <unistd.h>
 #include <pthread.h>
 
-/*SENARIO 1: ANASFALHS DIELEYSH ME SYGKROUSEIS.*/
+/*Scenario 1: Insecure crossing with collisions.*/
 
-#define threadsNum 4 /*ARITHMOS NHMATWN(AYTOKINHTWN) POU THELW NA DHMIOURGHSW*/
+#define threadsNum 4 /*Total number of threads*/
 
 typedef struct {
-    int *counter; /*DEIKTHS STON KOINO METRHTH.*/
-    long *bDelay; /*DEIKTHS GIA TON APAITOUMENO  XRONO DIELEFDHS THS GEFYRAS. */
-    pthread_t tID; /*KATAXWRHSH TOU NHMATOS(AYTOKINHTO) STO SYSTHMA ME TO ID TOU.*/
+    int *counter; /*Pointer to shared counter*/
+    long *bDelay; /*Pointer to the required time(delay) to cross the bridge*/
+    pthread_t tID; /*Threads ID(number)*/
 } thread_data;
 
-/*SYNARTHSH: KOKKINA AYTOKINHTA STHN GEFYRA.*/
+/*Function: Red cars on the bridge*/
 static void *redCarFunc(void *vargp)
 {
-    thread_data *data = (thread_data *)vargp; /*DHLWSH DEIKTH STHN DOMH 'thread_data'.*/
+    thread_data *data = (thread_data *)vargp; /*Pointer to struct: 'thread_data'*/
 
-    printf("Red Car %d arrived at time %d.\n", data->tID, *data->counter); /*TO AYTOKINHTO FTANEI STHN AKRH THS GEFYRAS.*/
+    printf("Red Car %d arrived at time %d.\n", data->tID, *data->counter); /*The car reaches the edge of the bridge.*/
     ++*data->counter;
-    printf("Red Car %d passing at time %d.\n", data->tID, *data->counter); /*TO AYTOKINHTO DIASXIZEI THN GEFYRA.*/
+    printf("Red Car %d passing at time %d.\n", data->tID, *data->counter); /*The car is crossing the bridge.*/
     *data->counter+=*data->bDelay;
-    printf("Red Car %d passed at time %d.\n", data->tID, *data->counter); /*TO AYTOKINHTO FTANEI STHN ALLH AKRH THS GEFYRAS.*/
+    printf("Red Car %d passed at time %d.\n", data->tID, *data->counter); /*The car reaches the other edge of the bridge.*/
     ++*data->counter;
 
 	return NULL;
 }
 
-/*SYNARTHSH: MPLE AYTOKINHTA STHN GEFYRA.*/
+/*Function: Blue cars on the bridge*/
 static void *blueCarFunc(void *vargp)
 {
-    thread_data *data = (thread_data *)vargp; /*DHLWSH DEIKTH STHN DOMH 'thread_data'.*/
+    thread_data *data = (thread_data *)vargp; /*Pointer to struct: 'thread_data'*/
 
-    printf("%55s %d %15s %d.\n", "Blue Car", data->tID, "arrived at time", *data->counter); /*TO AYTOKINHTO FTANEI STHN AKRH THS GEFYRAS.*/
+    printf("%55s %d %15s %d.\n", "Blue Car", data->tID, "arrived at time", *data->counter); /*The car reaches the edge of the bridge.*/
     ++*data->counter;
-    printf("%55s %d %15s %d.\n", "Blue Car", data->tID, "passing at time", *data->counter); /*TO AYTOKINHTO DIASXIZEI THN GEFYRA.*/
+    printf("%55s %d %15s %d.\n", "Blue Car", data->tID, "passing at time", *data->counter); /*The car is crossing the bridge.*/
     *data->counter+=*data->bDelay;
-    printf("%55s %d %14s %d.\n", "Blue Car", data->tID, "passed at time", *data->counter); /*TO AYTOKINHTO FTANEI STHN ALLH AKRH THS GEFYRAS.*/
+    printf("%55s %d %14s %d.\n", "Blue Car", data->tID, "passed at time", *data->counter); /*The car reaches the other edge of the bridge.*/
     ++*data->counter;
 
 	return NULL;
 }
 
 
-/*KYRIWS PROGRAMMA*/
+/*Main Program*/
 int main(int argc, char *argv[])
 {
     int i;
     int cnt;
     long delay;
 
-    /*DHLWSH NHMATWN(KOKKINA KAI MPLE AYTOKINHTA).*/
+    /*Threads declaration(Red & Blue cars)*/
 	thread_data threadDataCar[threadsNum];
 
-	cnt=0; /*ARXIKOPOIHSH KOINOU METRHTH.*/
-	delay=10; /*ARXIKOPOIHSH XRONOU POU APAITHTAI GIA THN DIASXISH THS GEFYRAS.*/
+	cnt=0; /*Initialization of shared counter*/
+	delay=10; /*Initialization of the required time(delay) to cross the bridge*/
 
-	/*BASIKES EKTYPWSEIS.*/
+	/*Print Bridge Information*/
 	printf("%40s\n", "Bridge");
 	printf("%20s", "Left Side");
 	printf("%45s\n", "Right Side");
 
-	/*DHMIOURGIA NHMATWN KAI KLHSH THS KATALLHLHS SYNARTHSHS ANALOGA ME THN SYNTHIKH POU ISXUEI.*/
+	/*Create threads and call the appropriate function, by the if() condition*/
 	for (i=0; i<threadsNum; i++){
 
-        void *thread_func; /*PROSWRINH SYNARTHSH*/
+        void *thread_func; /*Temporary function*/
 
-        /*SYSXETISH METRHTWN ME THN DOMH TWN NHMATWN.*/
+        /*Correlating local variables with struct's(thread_data) variables*/
         threadDataCar[i].counter=&cnt;
         threadDataCar[i].bDelay=&delay;
         threadDataCar[i].tID = i;
 
 		if (threadDataCar[i].tID%2!=0) {
-			thread_func = redCarFunc; /*AN TO AYTOKINHTO EINAI KOKKINO.*/
+			thread_func = redCarFunc; /*If car is red, choose redCarFunc*/
 		}
 		else {
-			thread_func = blueCarFunc; /*AN TO AYTOKINHTO EINAI MPLE.*/
+			thread_func = blueCarFunc; /*If car is blue, choose blueCarFunc*/
 		}
-		/*DHMIOURGIA NHMATOS KAI KLHSH THS KATALLHLHS SYNARTHSHS.*/
+		/*Create thread and call the correct function*/
         pthread_create(&threadDataCar[i].tID, NULL, thread_func, &threadDataCar[i]);
 	}
 
-	/*ANAMONH GIA TERMATISMO OLWN TWN NHMATWN.*/
+	/*Wait all threads to end*/
 	for (i=0; i<threadsNum; i++){
         pthread_join(threadDataCar[i].tID, NULL);
 	}
 
-    /*TERMATISMOS PROGRAMMATOS.*/
+    /*Close Program*/
 	exit(0);
 }
 
